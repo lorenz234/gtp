ff646d298ff1383e493a56bec7b4b1255c048a9a
lorenz234 <lorenz_lehmann@yahoo.de>
2025-05-26 14:30:39 +0200

    add mev prefilter

--- backend/src/misc/airflow_utils.py
+++ backend/src/misc/airflow_utils.py
@@ -16,6 +16,8 @@ def alert_via_webhook(context, user='mseidl'):
         user_id = '326358477335298050'
     elif user == 'mike':
         user_id = '253618927572221962'
+    elif user == 'ahoura':
+        user_id = '874921624720257037'
 
     message = f"<@{user_id}> -- A failure occurred in {dag_run.dag_id} on task {task_instance.task_id}. Might just be a transient issue -- Exception: {exception}"
     send_discord_message(message[:499], webhook_url)
