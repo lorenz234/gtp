ce2c7600498dea3009924554a80011af96ae9af5
Matthias Seidl <matthias@mseidl-analytics.de>
2025-06-05 09:36:36 +0200

    changed eigenda alert user

--- backend/airflow/dags/metrics/metrics_eigenDA.py
+++ backend/airflow/dags/metrics/metrics_eigenDA.py
@@ -13,7 +13,7 @@ from src.misc.airflow_utils import alert_via_webhook
         'retries': 1,
         'email_on_failure': False,
         'retry_delay': timedelta(minutes=5),
-        'on_failure_callback': alert_via_webhook
+        'on_failure_callback': lambda context: alert_via_webhook(context, user='lorenz')
     },
     dag_id='metrics_eigenda',
     description='Load data from EigenDA API.',
