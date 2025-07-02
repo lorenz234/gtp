cbdd2bec25fcd678cbae83ea92a89e8efb66a7e9
Matthias Seidl <matthias@mseidl-analytics.de>
2025-07-02 18:47:13 +0200

    correct app fees (manipulate ethereum circle/tether fees, remove arbitrum timeboost)

--- backend/src/adapters/adapter_defillama.py
+++ backend/src/adapters/adapter_defillama.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from src.adapters.abstract_adapters import AbstractAdapter
 from src.main_config import get_main_config
-from src.misc.helper_functions import api_get_call, return_projects_to_load, check_projects_to_load, get_df_kpis, upsert_to_kpis, get_missing_days_kpis, send_discord_message
+from src.misc.helper_functions import api_get_call, return_projects_to_load, check_projects_to_load, upsert_to_kpis
 from src.misc.helper_functions import print_init, print_load, print_extract
 
 class AdapterDefillama(AbstractAdapter):
@@ -44,34 +44,103 @@ class AdapterDefillama(AbstractAdapter):
     ## ----------------- Helper functions --------------------
 
     ## TODO: 
-    ## - Remove circle and tether fees partially (not all is on Ethereum). See notebook!
     ## - Remove network fees (i.e. Arbitrum REV)
     def extract_app_fees(self, projects_to_load):
         df_main = pd.DataFrame()
         for chain in projects_to_load:
             alias = chain.aliases_defillama
             origin_key = chain.origin_key
+            
+            print(f'..processing {origin_key} with alias {alias}')
 
             url = f'{self.base_url}overview/fees/{alias}?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=false&dataType=dailyFees'
-            print(f'Fetching: {url}')
+            print(f'..fetching: {url}')
             response_json = api_get_call(url)
 
             # Build a list of dicts, then create the DataFrame at once (avoiding deprecated .append)
+            if len(response_json['totalDataChartBreakdown']) == 0:
+                print(f'No data found for {origin_key}. Skipping...')
+                continue
+                
             rows = []
             for item in response_json['totalDataChartBreakdown']:
-                rows.append({
-                    'unix': item[0],
-                    'value': sum(item[1].values())
-                })
+                for key, value in item[1:][0].items():
+                    rows.append({
+                        'unix': item[0],
+                        'protocol': key,
+                        'value': value
+                    })
             df = pd.DataFrame(rows)
             df['origin_key'] = origin_key
+            df['date'] = pd.to_datetime(df['unix'], unit='s')
+            df = df.sort_values(by='date')
+            
+            # For Ethereum, we need to correct Tether and Circle fees
+            if origin_key == 'ethereum':
+                # load the stables data, merge it with the current df, calculate eth_dominance, calculate the adjusted fees
+                df_stables = self.load_stables_df()
+                df = pd.merge(df, df_stables[['date', 'protocol', 'eth_dominance']], on=['date', 'protocol'], how='left')
+                df.loc[df['protocol'].isin(['Tether', 'Circle']), 'value'] *= df['eth_dominance']
+            
+            # For Arbitrum, we need to remove Timeboost (since it already included in the chain REV/revenue)
+            if origin_key == 'arbitrum':
+                df = df[~df['protocol'].isin(['Timeboost'])]
+            
             df_main = pd.concat([df_main, df], ignore_index=True)
             time.sleep(1)  # Respect API rate limits
 
-        df_main['date'] = pd.to_datetime(df_main['unix'], unit='s')
-        df_main = df_main.sort_values(by='date')
+            
         df_main = df_main.drop(columns=['unix'])
+        
+        ## aggregate by origin_key, date (this will drop the protocol column -> might be interesting for later though on app level)
+        df_main = df_main.groupby(['origin_key', 'date']).agg({'value': 'sum'}).reset_index()
         df_main['metric_key'] = 'app_fees_usd'
 
         df_main.set_index(['date', 'origin_key', 'metric_key'], inplace=True)
         return df_main
+    
+    def load_stables_df(self):
+        stables = {
+            'Tether': 1,
+            'Circle': 2,
+        }
+
+        df_stables = pd.DataFrame()
+        for name, id in stables.items():
+            url = f"https://stablecoins.llama.fi/stablecoin/{id}"
+            print(f'..fetching: {url}')
+            response_json = api_get_call(url)
+
+            # Build a list of dicts, then create the DataFrame at once (avoiding deprecated .append)
+            rows = []
+            for item in response_json['tokens']:
+                rows.append({
+                    'unix': int(item['date']),
+                    'circ_total': item['circulating']['peggedUSD']
+                })
+            df = pd.DataFrame(rows)
+
+            time.sleep(1)  # To avoid hitting the API too hard
+
+            url = f"https://stablecoins.llama.fi/stablecoincharts/Ethereum?stablecoin={id}"
+            print(f'..fetching: {url}')
+            response_json = api_get_call(url)
+            rows = []
+            for item in response_json:
+                rows.append({
+                    'unix': int(item['date']),
+                    'circ_ethereum': item['totalCirculating']['peggedUSD']
+                })
+            df2 = pd.DataFrame(rows)
+
+            df = pd.merge(df, df2, on='unix', how='outer')
+            df['protocol'] = name
+            df_stables = pd.concat([df_stables, df], ignore_index=True)
+
+            time.sleep(1)  # To avoid hitting the API too hard
+
+        df_stables['date'] = pd.to_datetime(df_stables['unix'], unit='s')
+        df_stables = df_stables.sort_values(by='date')
+        df_stables['eth_dominance'] = df_stables['circ_ethereum'] / df_stables['circ_total']
+
+        return df_stables
