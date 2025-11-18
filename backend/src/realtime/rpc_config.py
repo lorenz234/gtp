92bf681e4bfee9840910fbee487529eb928e79c9
Matthias Seidl <matthias@mseidl-analytics.de>
2025-11-18 13:48:07 +0100

    [realtime tps] add gravity

--- backend/src/realtime/rpc_config.py
+++ backend/src/realtime/rpc_config.py
@@ -143,6 +143,14 @@ rpc_config = {
         "calc_fees": False, ##custom gas?
     },
     
+    "gravity": {
+        "name": "Gravity",
+        "processors": "evm",
+        "stack": "nitro",
+        "sleeper": 3,
+        "calc_fees": False,
+    },
+    
     # ZK Stack chains
     "zksync_era": {
         "name": "ZKsync Era",
