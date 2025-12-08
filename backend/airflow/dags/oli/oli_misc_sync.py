ed1d608ce8e482b953124b2ae065f2bad826c29a
lorenz234 <lorenz_lehmann@yahoo.de>
2025-12-08 16:30:27 +0100

    add trust_list to ipfs

--- backend/airflow/dags/oli/oli_misc_sync.py
+++ backend/airflow/dags/oli/oli_misc_sync.py
@@ -42,3 +42,46 @@
         # upsert to oli_trusted_entities table, making sure to delete first for full refresh
         db_connector.delete_all_rows('oli_trusted_entities')
         db_connector.upsert_table('oli_trusted_entities', df)
+
+
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+from src.misc.airflow_utils import alert_via_webhook
+
+# Define the DAG and task using decorators
+@dag(
+    default_args={
+        'owner': 'lorenz',
+        'retries': 5,
+        'email_on_failure': False,
+        'retry_delay': timedelta(minutes=5),
+        'on_failure_callback': lambda context: alert_via_webhook(context, user='lorenz')
+    },
+    dag_id='oli_label_pool',
+    description='Loads raw labels into the data pool',
+    tags=['contracts', 'oli', 'raw'],
+    start_date=datetime(2023, 6, 5),
+    schedule='*/30 * * * *',  # Runs every 30 minutes
+    catchup=False  # Prevents backfilling
+)
+
+def main():
+
+    @task()
+    def sync_attestations():
+
+        from src.db_connector import DbConnector
+        from src.adapters.adapter_oli_label_pool import AdapterLabelPool
+
+        db_connector = DbConnector()
+        ad = AdapterLabelPool({}, db_connector)
+
+        # get new labels from the GraphQL endpoint
+        df = ad.extract() # you can set from when to load [optional] e.g. {'time_created': 1758504151}
+        
+        # load labels into bronze table, then also increment to silver and lastly pushes untrusted owner_project labels to airtable
+        ad.load(df)
+
+    sync_attestations()
+
+main()
