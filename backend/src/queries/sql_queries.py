9e6a9b3ff1f40455e579d9fd3e44f0bf4a08493d
Matthias Seidl <matthias@mseidl-analytics.de>
2025-12-31 15:04:24 +0100

    [sql queries] deactivate Polygon zkEVM agg

--- backend/src/queries/sql_queries.py
+++ backend/src/queries/sql_queries.py
@@ -162,21 +162,21 @@ sql_queries = [
         ,*standard_evm_queries("zksync_era")
 
         # Polygon zkStack
-        ## Polygon zkEVM - standard but txcount_comparison is plain txcount
-        ,SQLQuery(metric_key = "txcount_comparison", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcount_plain.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "txcount", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcount.sql.j2', query_parameters={"filter_col" : "gas_price"}, currency_dependent = False)
-        ,SQLQuery(metric_key = "txcount_plain", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcount_plain.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "daa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_daa.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "waa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_waa.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "maa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_maa.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "qaa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_qaa.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "aa_last7d", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_aa_lastXXd.sql.j2', query_parameters={"timerange" : 7}, currency_dependent = False)
-        ,SQLQuery(metric_key = "aa_last30d", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_aa_lastXXd.sql.j2',query_parameters={"timerange" : 30}, currency_dependent = False)
-        ,SQLQuery(metric_key = "gas_per_second", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_gas_per_second.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "cca_last7d_exclusive", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_cca_last7d.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "cca_weekly_exclusive", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_cca_weekly.sql.j2', currency_dependent = False)
-        ,SQLQuery(metric_key = "fees_paid_eth", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_fees_paid.sql.j2')
-        ,SQLQuery(metric_key = "txcosts_median_eth", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcosts_median.sql.j2')
+        # ## Polygon zkEVM - standard but txcount_comparison is plain txcount
+        # ,SQLQuery(metric_key = "txcount_comparison", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcount_plain.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "txcount", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcount.sql.j2', query_parameters={"filter_col" : "gas_price"}, currency_dependent = False)
+        # ,SQLQuery(metric_key = "txcount_plain", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcount_plain.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "daa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_daa.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "waa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_waa.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "maa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_maa.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "qaa", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_qaa.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "aa_last7d", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_aa_lastXXd.sql.j2', query_parameters={"timerange" : 7}, currency_dependent = False)
+        # ,SQLQuery(metric_key = "aa_last30d", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_aa_lastXXd.sql.j2',query_parameters={"timerange" : 30}, currency_dependent = False)
+        # ,SQLQuery(metric_key = "gas_per_second", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_gas_per_second.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "cca_last7d_exclusive", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_cca_last7d.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "cca_weekly_exclusive", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_cca_weekly.sql.j2', currency_dependent = False)
+        # ,SQLQuery(metric_key = "fees_paid_eth", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_fees_paid.sql.j2')
+        # ,SQLQuery(metric_key = "txcosts_median_eth", origin_key = "polygon_zkevm", jinja_path='chain_metrics/select_txcosts_median.sql.j2')
 
         # Others EVM
         ## Linea
