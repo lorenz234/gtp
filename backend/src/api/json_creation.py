889f365a48a1abed458872351f02dd5b934a01a0
Matthias Seidl <matthias@mseidl-analytics.de>
2026-01-15 10:09:11 +0100

    [OLI caip 10] fix checksum

--- backend/src/api/json_creation.py
+++ backend/src/api/json_creation.py
@@ -3075,7 +3075,8 @@ class JSONCreation():
         """
         with self.db_connector.engine.connect() as connection:
             df = pd.read_sql(exec_string, connection)
-        df = db_addresses_to_checksummed_addresses(df, ['attester', 'recipient'])
+        df = db_addresses_to_checksummed_addresses(df, ['attester'])
+        df = string_addresses_to_checksummed_addresses(df, ['recipient'])
 
         upload_parquet_to_cf_s3(self.s3_bucket, f'{self.api_version}/oli/labels_raw', df, self.cf_distribution_id)
         print(f'DONE -- OLI labels_raw.parquet export')
@@ -3086,7 +3087,8 @@ class JSONCreation():
         """
         with self.db_connector.engine.connect() as connection:
             df = pd.read_sql(exec_string, connection)
-        df = db_addresses_to_checksummed_addresses(df, ['address', 'attester'])
+        df = db_addresses_to_checksummed_addresses(df, ['attester'])
+        df = string_addresses_to_checksummed_addresses(df, ['address'])
 
         upload_parquet_to_cf_s3(self.s3_bucket, f'{self.api_version}/oli/labels_decoded', df, self.cf_distribution_id)
         print(f'DONE -- OLI labels_decoded.parquet export')
