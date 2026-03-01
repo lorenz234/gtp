62091cb1de032497625a66ec35893ec90e7ba4a5
Matthias Seidl <matthias@mseidl-analytics.de>
2026-03-01 12:30:44 +0100

    [4 byte] only 2 retries

--- backend/airflow/dags/utility/utility_4byte.py
+++ backend/airflow/dags/utility/utility_4byte.py
@@ -6,7 +6,7 @@ from src.misc.airflow_utils import alert_via_webhook
 @dag(
     default_args={
         'owner' : 'lorenz',
-        'retries' : 5,
+        'retries' : 2,
         'email_on_failure': False,
         'retry_delay' : timedelta(seconds=5),
         'on_failure_callback': lambda context: alert_via_webhook(context, user='lorenz')
